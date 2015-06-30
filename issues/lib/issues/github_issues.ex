17cb65f668a32deaf7f2f690836d7a3f2c2be27f
zigomir <zigomir@gmail.com>
2015-06-30 18:36:33 +0200

    Code monkeying.

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,16 +1,23 @@
 defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir zigomir@gmail.com"}]
+  @github_url Application.get_env(:issues, :github_url)
 
+  # To test this run `iex -S mix`
+  # Issues.GithubIssues.fetch("elixir-lang", "elixir")
   def fetch(user, project) do
     issues_url(user, project)
-    |> HTTPoison.get(@user_agent)
+    |> HTTPoison.get!(@user_agent) # find out why .get (without !) won't work
     |> handle_response
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({%{status_code: 200, body: body}}), do: {:ok, body}
-  def handle_response({%{status_code: ___, body: body}}), do: {:error, body}
+  def handle_response(%HTTPoison.Response{status_code: 200, body: body}) do
+    {:ok, :jsx.decode(body)}
+  end
+  def handle_response(%HTTPoison.Response{status_code: ___, body: body}) do
+    {:error, :jsx.decode(body)}
+  end
 end
--- issues/mix.exs
+++ issues/mix.exs
@@ -28,7 +28,8 @@ defmodule Issues.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.7"}
+      {:httpoison, "~> 0.7"},
+      {:jsx,       "~> 2.6.2"}
     ]
   end
 end
